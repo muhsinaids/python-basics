4bfd5e1e928cb3400edca888095d92b8cbebc1f6
muhsinaids <muhsinbinmubarak11@gmail.com>
2026-02-01 16:28:14 +0530

    string multiple, concatenation, indexing.

--- string_methods/string_methods.py
+++ string_methods/string_methods.py
@@ -2,10 +2,12 @@ first_name = "MUHAMMED"
 last_name = "MUHSIN"
 full_name = first_name + " " + last_name
 print(full_name)
+
 text = "Python Programming"
 print(text * 3)
 additional = " is awesome."
 text += additional
+
 print(text)
 print(text[:10])
 print(text[0:6])
