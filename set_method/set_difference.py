74ec80c7f90c918298fdc157f32890d240580225
muhsinaids <muhsinbinmubarak11@gmail.com>
2026-03-24 13:40:28 +0530

    practice

---
+++ set_method/set_difference.py
@@ -0,0 +1,4 @@
+set_A={'A',2,4,'D'}
+set_B={'A','B','C','D'}
+C=set_A-set_B
+print("The Difference:",C)
--- set_method/set_intersection.py
+++ set_method/set_intersection.py
@@ -1,4 +1,4 @@
 set_A={'A',2,4,'D'}
 set_B={'A','B','C','D'}
 C=set_A&set_B
-print(C)
+print("The Intersection:",C)
